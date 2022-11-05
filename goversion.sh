809f3e189b17b152a946bdb11dfaf1965db292bc
Mark Pruett <67116121+markapruett@users.noreply.github.com>
2022-11-04 19:03:08 -0500

    Create goversion.sh

---
+++ goversion.sh
@@ -0,0 +1,5 @@
+#   - name: get-current-golang-ver
+#       run: |
+#         CURRENT_GOLANG_VER=$( curl -s https://go.dev/VERSION?m=text | sed -r 's/^.{2}//' )
+#         echo $CURRENT_GOLANG_VER
+  
